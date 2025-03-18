13dbf0da7d224015a9018490ab99ee57ac6aba77
Maryam-Sargol <sargol@chalmers.se>
2025-03-19 00:46:56 +0100

    revise read_extract_nodes_shells

--- PCA_morphed_model/read_extract.py
+++ PCA_morphed_model/read_extract.py
@@ -20,20 +20,22 @@ def read_extract_nodes_shells(input_dir:str  ):  # input directory
 
     # extract the cordinates of nodes of all morphed files
     nodes_all_morphed_files = []
+    shells = np.empty(0)
+    id_nodes = []
 
-    for file_path in all_morphed_file_pathes:
-        nodes1 = extract_nodes_shells_mix_stl_key(file_path)[0]
-        
-        nodes_without_id = [sublist[1:] for sublist in nodes1]    # extract nodes cordinates (x, y, z)
-        id_nodes = [sublist[0] for sublist in nodes1]             # extract id of nodes
-        nodes_list = [item for sublist in nodes_without_id for item in sublist]     # Convert to one list
-        nodes_all_morphed_files.append(nodes_list)                # get nodes of all morphed files
-        
     if all_morphed_file_pathes != []:
+        for file_path in all_morphed_file_pathes:
+            nodes1 = extract_nodes_shells_mix_stl_key(file_path)[0]
+            
+            nodes_without_id = [sublist[1:] for sublist in nodes1]    # extract nodes cordinates (x, y, z)
+            id_nodes = [sublist[0] for sublist in nodes1]             # extract id of nodes
+            nodes_list = [item for sublist in nodes_without_id for item in sublist]     # Convert to one list
+            nodes_all_morphed_files.append(nodes_list)                # get nodes of all morphed files
+        
         file_extension = extract_nodes_shells_mix_stl_key(all_morphed_file_pathes[-1])[2]
         print(f"{len(all_morphed_file_pathes)} {file_extension} files were read")
         shells = extract_nodes_shells_mix_stl_key(all_morphed_file_pathes[-1])[1]
-
+    
     data_array = np.array(nodes_all_morphed_files) 
-
     return data_array, shells, id_nodes
+    
